84092ec4ac9e49129d264c60a3fffde5db5158eb
Osiel-Paredes <oosielparedes@gmail.com>
2024-12-04 09:02:06 -0600

    Practica 25, scroll vertical

--- toktik_210661/lib/presentation/widgets/shared/video_scrollable_view.dart
+++ toktik_210661/lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toktik_210661/domains/entities/video_post.dart';
+import 'package:toktik_210661/presentation/widgets/shared/video_buttons.dart';
 
 class VideoScrollableView extends StatelessWidget {
   final List<VideoPost> videos;
@@ -7,12 +8,23 @@ class VideoScrollableView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
-      children: [
-        Container(
-          color: Colors.red,
-        )
-      ],
-    );
+    return PageView.builder(
+        scrollDirection: Axis.vertical,
+        physics: const BouncingScrollPhysics(),
+        itemCount: videos.length,
+        itemBuilder: (context, index) {
+          final VideoPost videoPost = videos[index];
+
+          return Stack(
+            children: [
+              // Video
+
+              Positioned(
+                bottom: 40,
+                right: 20,
+                child: VideoButtons(video: videoPost))
+            ],
+          );
+        });
   }
 }
